e51c93c8f4cf766fd52224adeac6383c273c5248
lgf <910683417@qq.com>
2018-01-25 16:46:08 +0800

    添加Stetho调试工具

--- app/src/main/java/com/example/lgf/toutiao/InitApp.java
+++ app/src/main/java/com/example/lgf/toutiao/InitApp.java
@@ -3,6 +3,8 @@ package com.example.lgf.toutiao;
 import android.content.Context;
 import android.support.multidex.MultiDexApplication;
 
+import com.facebook.stetho.Stetho;
+
 /**
  * 如果minSdkVersion 设置为 20 或更低值，要使应用进行 Dalvik 可执行文件分包，需继承MultiDexApplication(需添加依赖：com.android.support:multidex:1.0.2)
  */
@@ -13,5 +15,9 @@ public class InitApp extends MultiDexApplication {
     public void onCreate() {
         super.onCreate();
         context = getApplicationContext();
+        if (BuildConfig.DEBUG) {
+            // Chrome中输入-->chrome://inspect，然后点击inspect
+            Stetho.initializeWithDefaults(this);
+        }
     }
 }

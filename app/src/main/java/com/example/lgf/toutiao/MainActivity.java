0aef181ab2ceb6a2142e8b3340bd8c499f4cdbb7
lgf <910683417@qq.com>
2018-01-26 14:49:29 +0800

    添加BottomNavigationView以及BottomNavigationView菜单空间均分设置

--- app/src/main/java/com/example/lgf/toutiao/MainActivity.java
+++ app/src/main/java/com/example/lgf/toutiao/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.lgf.toutiao;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.lgf.toutiao.widget.helper.BottomNavigationViewHelper;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.inflateMenu(R.menu.menu_main_toolbar);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
     }
 }
